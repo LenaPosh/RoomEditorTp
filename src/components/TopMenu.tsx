910dc0c0202bb483f28b3b257771c2d99f459329
Lena <120247327+LenaPosh@users.noreply.github.com>
2024-02-13 23:53:37 +0300

    added mobile version of the top menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {ArrowIcon} from './Icon';
 import {GlobalStyle} from "./form/FormStyles";
@@ -7,6 +7,9 @@ import saveIcon from '../img/Save.png';
 import backIcon from '../img/Back.png';
 import {projectTitleText} from "../textVariables";
 import { useTranslation } from 'react-i18next';
+import logo from '../img/Logo VIM 1.png';
+import people from '../img/people.png';
+import burger from '../img/radix-icons_hamburger-menu.png';
 
 
 const MenuContainer = styled.div`
@@ -85,15 +88,61 @@ const LanguageSwitcherButton = styled.button`
   }
 `;
 
+const MenuContainerMobile = styled.div`
+  display: none;
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    height: 60px;
+    align-items: center;
+    background-color: #fff;
+    padding: 0 10px;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  }
+`;
 
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+`
+const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  margin-right: 10px;
+`
+
+const BurgerIcon = styled.img`
+  width: 25px;
+  height: 25px;
+  order: 3;
+`
+
+const LogoImg = styled.img`
+  width: 45px;
+  height: 30px;
+  order: 1;
+`
+const PeopleIcon = styled.img`
+  width: 18px;
+  height: 19px;
+  order: 2;  
+`
 
 const TopMenu = () => {
     const { t, i18n } = useTranslation();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleLanguage = () => {
         const nextLanguage = i18n.language === 'en' ? 'ru' : 'en';
         i18n.changeLanguage(nextLanguage);
     };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
 
     return (
         <>
@@ -116,6 +165,21 @@ const TopMenu = () => {
                     </LanguageSwitcherButton>
                 </MenuRight>
             </MenuContainer>
+            <MenuContainerMobile>
+                <LogoContainer>
+                    <LogoImg src={logo} alt="Logo" />
+                </LogoContainer>
+                <IconsContainer>
+                    <BurgerIcon src={burger} alt="Menu" onClick={toggleMenu} />
+                    <PeopleIcon src={people} alt="People" />
+                    <LanguageSwitcherButton onClick={toggleLanguage}>
+                        {i18n.language === 'en' ? 'RU' : 'EN'}
+                    </LanguageSwitcherButton>
+                </IconsContainer>
+            </MenuContainerMobile>
+            {/*{menuOpen && (*/}
+
+            {/*)}*/}
         </>
 
     );
